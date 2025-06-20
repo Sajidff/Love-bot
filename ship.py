6bde3bdbe316079e666f7365434d3d55a0376cc8
Sajidff <tyryr1243@gmail.com>
2025-06-20 07:34:21 +0530

    Delete ship.py

--- ship.py
+++
@@ -1,8 +0,0 @@
-import random
-from pyrogram import filters
-def load(app):
-    @app.on_message(filters.command("ship"))
-    def ship_users(_, msg):
-        if len(msg.command) >= 3:
-            name1, name2 = msg.command[1], msg.command[2]
-            msg.reply_text(f"🚢 Shipping {name1} ❤️ {name2}... {random.randint(50, 100)}% Match!")
