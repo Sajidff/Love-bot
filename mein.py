5506e35beaa44dbf2598cd7bd2edf8662d2edeb8
Sajidff <tyryr1243@gmail.com>
2025-06-20 07:11:39 +0530

    Update mein.py

--- mein.py
+++ mein.py
@@ -1,1 +1,14 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+from plugins import love, ship, actions, coupleofday
 
+app = Client("lovebot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+# Load handlers
+love.load(app)
+ship.load(app)
+actions.load(app)
+coupleofday.load(app)
+
+print("Bot started...")
+app.run()
